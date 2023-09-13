ebf00442444c1fca596244c2980d3eaacc31229a
Ken <keniwane@gmail.com>
2023-09-13 15:19:59 -0700

    added new route

--- server/routes/api.js
+++ server/routes/api.js
@@ -10,9 +10,6 @@ const {
 // Route to search for ingredients
 apiRouter.get('/search-ingredients', searchIngredients);
 
-// Route to search for recipes based on ingredients
-// apiRouter.get('/search-recipes', searchRecipes);
-
 apiRouter.get('/find-recipes-by-ingredients', findRecipesByIngredients);
 
 apiRouter.get('/recipe-details/:id', getRecipeDetails);
