87fae43204af3876748762f8994cced9e5fe5253
Ken <keniwane@gmail.com>
2023-09-14 12:36:27 -0700

    added new schema

--- server/models/recipeModels.js
+++ server/models/recipeModels.js
@@ -2,6 +2,36 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const IngredientResultSchema = new Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const IngredientSearchSchema = new Schema({
+  query: {
+    type: String,
+    required: true,
+    description: 'The ingredient name searched by the user.',
+  },
+  results: [IngredientResultSchema],
+  totalResults: {
+    type: Number,
+    required: true,
+  },
+});
+
+const IngredientSearch = mongoose.model('IngredientSearch', IngredientSearchSchema);
+
 const missedIngredientSchema = new mongoose.Schema({
   aisle: String,
   amount: Number,
@@ -15,8 +45,6 @@ const missedIngredientSchema = new mongoose.Schema({
   unitLong: String,
   unitShort: String,
 });
-
-const MissedIngredient = mongoose.model('MissedIngredient', missedIngredientSchema);
 
 const usedIngredientSchema = new mongoose.Schema({
   aisle: String,
@@ -32,8 +60,6 @@ const usedIngredientSchema = new mongoose.Schema({
   unitShort: String,
 });
 
-const UsedIngredient = mongoose.model('usedIngredient', usedIngredientSchema);
-
 const unusedIngredientSchema = new mongoose.Schema({
   aisle: String,
   amount: Number,
@@ -48,40 +74,55 @@ const unusedIngredientSchema = new mongoose.Schema({
   unitShort: String,
 });
 
-const UnusedIngredient = mongoose.model('unusedIngredient', unusedIngredientSchema);
-
-const stepEquipmentSchema = new mongoose.Schema({
+// Schema for equipment
+const EquipmentSchema = new Schema({
   id: Number,
-  image: String,
   name: String,
-  temperature: {
-    number: Number,
-    unit: String,
+  localizedName: String,
+  image: String,
+});
+
+// Schema for ingredients
+const IngredientSchema = new Schema({
+  id: Number,
+  name: String,
+  localizedName: String,
+  image: String,
+});
+
+// Schema for individual steps
+const StepSchema = new Schema({
+  number: {
+    type: Number,
+    required: true,
   },
-});
-
-const stepIngredientSchema = new mongoose.Schema({
-  id: Number,
-  image: String,
-  name: String,
-});
-
-const stepSchema = new mongoose.Schema({
-  equipment: [stepEquipmentSchema],
-  ingredients: [stepIngredientSchema],
-  number: Number,
-  step: String,
+  step: {
+    type: String,
+    required: true,
+  },
+  equipment: [EquipmentSchema],
+  ingredients: [IngredientSchema],
   length: {
     number: Number,
     unit: String,
   },
 });
 
-const recipeDetailSchema = new mongoose.Schema({
-  steps: [stepSchema],
+// Main schema for the recipe details
+const RecipeDetailSchema = new Schema({
+  recipeId: {
+    // to uniquely identify the recipe
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    default: '',
+  },
+  steps: [StepSchema],
 });
 
-const RecipeDetail = mongoose.model('RecipeDetail', recipeDetailSchema);
+const RecipeDetail = mongoose.model('RecipeDetail', RecipeDetailSchema);
 
 const recipeSchema = new Schema({
   id: Number,
@@ -101,8 +142,6 @@ const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = {
   Recipe,
-  MissedIngredient,
-  UsedIngredient,
-  UnusedIngredient,
   RecipeDetail,
+  IngredientSearch,
 };
