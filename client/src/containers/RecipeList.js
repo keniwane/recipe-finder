2fad395ac91ca5a3d297d6ea4c9984cd3dd1038c
Ken <keniwane@gmail.com>
2023-09-13 20:27:12 -0700

    changed props

--- client/src/containers/RecipeList.js
+++ client/src/containers/RecipeList.js
@@ -1,78 +1,57 @@
-// RecipeList.js
-
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedRecipe } from '../reducers/recipeSlice';
-import RecipeDetail from '../components/RecipeDetails';
+import RecipeDetails from '../components/RecipeDetails';
 import axios from 'axios';
 
 function RecipeList() {
   const dispatch = useDispatch();
   const recipes = useSelector((state) => state.recipes.recipes);
   const selectedRecipe = useSelector((state) => state.recipes.selectedRecipe);
-  const [showRecipeDetails, setShowRecipeDetails] = useState(false); // State to control recipe details visibility
+  const [showRecipeDetails, setShowRecipeDetails] = useState(false);
 
   const handleRecipeClick = async (recipe) => {
-    dispatch(setSelectedRecipe(recipe));
-
-    // Fetch the full recipe details from your server, which will in turn make a Spoonacular API call
     try {
       const response = await axios.get(`http://localhost:8000/api/recipe-details/${recipe.id}`);
+      console.log('Updated selectedRecipe:', response.data);
 
-      // Update the selectedRecipe with the full details
-      dispatch(setSelectedRecipe({ ...recipe, details: response.data }));
-      setShowRecipeDetails(true); // Show the recipe details
+      // Dispatch the new selected recipe to completely replace the existing one
+      dispatch(setSelectedRecipe(response.data));
+      setShowRecipeDetails(true);
     } catch (error) {
       console.error('Error fetching recipe details:', error);
     }
   };
-  console.log(selectedRecipe);
+
+  console.log('showRecipeDetails:', showRecipeDetails);
+  console.log('selectedRecipe:', selectedRecipe);
+  console.log('selectedRecipe.steps:', selectedRecipe && selectedRecipe[0].steps);
 
   return (
     <div>
-      <h2>Recipes</h2>
+      <h2 className='header'>Recipes</h2>
       <ul>
         {recipes.map((recipe) => (
-          <li key={recipe.id} onClick={() => handleRecipeClick(recipe)}>
+          <li key={recipe.id} onClick={() => handleRecipeClick(recipe)} className='recipe-item'>
             <h3>{recipe.title}</h3>
             <img src={recipe.image} alt={recipe.title} />
-            <p>Likes: {recipe.likes}</p>
+            <p>Missing Ingredients:</p>
+            <ul>
+              {recipe.missedIngredients && recipe.missedIngredients.length > 0 ? (
+                recipe.missedIngredients.map((ingredient) => (
+                  <li key={ingredient.id}>{ingredient.name}</li>
+                ))
+              ) : (
+                <li>None</li>
+              )}
+            </ul>
           </li>
         ))}
       </ul>
-      {showRecipeDetails && selectedRecipe && <RecipeDetail recipe={selectedRecipe} />}
+      {showRecipeDetails && selectedRecipe && <RecipeDetails recipe={selectedRecipe[0]} />}
+      {/* Pass the selected recipe as a prop */}
     </div>
   );
 }
-
-// function RecipeList() {
-//   const recipes = useSelector((state) => state.recipes);
-
-//   return (
-//     <div>
-//       <h2>Recipes</h2>
-//       <ul>
-//         {recipes.map((recipe) => (
-//           <li key={recipe.id}>
-//             <h3>{recipe.title}</h3>
-//             <img src={recipe.image} alt={recipe.title} />
-//             <p>Likes: {recipe.likes}</p>
-//             <h4>Missing Ingredients:</h4>
-//             <ul>
-//               {recipe.missedIngredients.length > 0 ? (
-//                 recipe.missedIngredients.map((ingredient) => (
-//                   <li key={ingredient.id}>{ingredient.name}</li>
-//                 ))
-//               ) : (
-//                 <li>None</li>
-//               )}
-//             </ul>
-//             {/* Similar lists for unusedIngredients and usedIngredients */}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
 
 export default RecipeList;
